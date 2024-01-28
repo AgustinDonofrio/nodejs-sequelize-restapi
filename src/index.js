70206ee127bfa243d6ce0beaa776e5474fb149ab
DOnofrioAgustin <adonofrio@alumnos.exa.unicen.edu.ar>
2024-01-28 01:26:46 -0300

    Sequelize Schemas

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import app from './app.js'
 import {sequelize} from './database/database.js'
+import './models/Project.js'
+import './models/Task.js'
 
 async function main() {
     try {
         await sequelize.authenticate();
         console.log('Connection has been established successfully');
+        await sequelize.sync({force: true})
         app.listen(3000);
         console.log('Server running on port', 3000);
     }
